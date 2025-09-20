2f9908b51102bb0d1930437ce681ef971fbc4f19
flutter-lib <flutterlib@gmail.com>
2025-09-21 01:47:20 +0800

    feat(android): 添加 VPN 服务支持和相关权限配置新增 ClashSingVpnService 以支持 VPN 功能，并在 AndroidManifest.xml 中声明了必要权限。同时启用了 desugaring 支持以兼容较旧的 Android 版本，并引入了 singbox-android 库。调整了 Flutter 插件逻辑以启动前台服务。

---
+++ android/src/main/kotlin/com/clashsiing/flutter_sing_box/ClashSingVpnService.kt
@@ -0,0 +1,87 @@
+package com.clashsiing.flutter_sing_box
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.net.VpnService
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+@SuppressLint("VpnServicePolicy")
+class ClashSingVpnService : VpnService() {
+    private var serviceScope: CoroutineScope? = null
+    private val notificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
+    private val notificationBuilder by lazy {
+        NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(android.R.mipmap.sym_def_app_icon)
+            .setContentTitle("前台服务正在运行")
+            .setContentText("点击可返回应用")
+    }
+    companion object {
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "ForegroundServiceChannel"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        if (intent?.action == "STOP_SERVICE") {
+            stopSelf()
+            return START_NOT_STICKY
+        } else {
+            if (serviceScope?.isActive != true) {
+                doHeavyWork()
+            }
+            return START_STICKY
+        }
+    }
+
+    private fun doHeavyWork() {
+        if (serviceScope?.isActive != true) {
+            serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+            serviceScope?.launch {
+                while (isActive) {
+                    delay(1000)
+                    notificationManager.notify(
+                        NOTIFICATION_ID,
+                        notificationBuilder.setContentText("Time: ${System.currentTimeMillis()}").build()
+                    )
+                    println("Foreground Service is running... ${System.currentTimeMillis()}")
+                }
+                // 在这里执行你的耗时任务
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "前台服务通知",
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                description = "用于前台服务持续运行的通知"
+            }
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
+    override fun onDestroy() {
+        serviceScope?.cancel()
+        serviceScope = null
+        super.onDestroy()
+    }
+}
--- android/src/main/kotlin/com/clashsiing/flutter_sing_box/FlutterSingBoxPlugin.kt
+++ android/src/main/kotlin/com/clashsiing/flutter_sing_box/FlutterSingBoxPlugin.kt
@@ -1,5 +1,6 @@
 package com.clashsiing.flutter_sing_box
 
+import android.content.Intent
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -15,20 +16,43 @@ class FlutterSingBoxPlugin :
     // This local reference serves to register the plugin with the Flutter Engine and unregister it
     // when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+    private lateinit var pluginBinding: FlutterPlugin.FlutterPluginBinding
+    private val applicationContext by lazy { pluginBinding.applicationContext }
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sing_box")
         channel.setMethodCallHandler(this)
+        pluginBinding = flutterPluginBinding
     }
 
     override fun onMethodCall(
         call: MethodCall,
         result: Result
     ) {
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
+        when (call.method) {
+            "getPlatformVersion" -> {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    applicationContext.startForegroundService(
+                        Intent(
+                            pluginBinding.applicationContext,
+                            ClashSingVpnService::class.java
+                        )
+                    )
+
+                } else {
+                    applicationContext.startService(
+                        Intent(
+                            pluginBinding.applicationContext,
+                            ClashSingVpnService::class.java
+                        )
+                    )
+                }
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
+
+            else -> {
+                result.notImplemented()
+            }
         }
     }
 
