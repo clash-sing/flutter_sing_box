4206f47cda08ff9bd9da7ead3514f8c123402b03
flutter-lib <flutterlib@gmail.com>
2025-10-04 22:20:19 +0800

    chore(build): 设置根项目名称并包含应用模块- 添加了 rootProject.name 配置，设置项目名为 "csExample"- 使用 include(":app") 包含应用模块 - 更新了 settings.gradle.kts 文件结构

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -22,5 +22,5 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
-
+rootProject.name = "csExample"
 include(":app")
