3d15c27438ea5879c51eec179ecab07a4b3321fe
flutter-lib <flutterlib@gmail.com>
2026-01-19 01:09:02 +0800

    feat: Update package version to 1.0.0 and stage test file.

---
+++ test/flutter_sing_box_method_channel_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  // MethodChannelFlutterSingBox platform = MethodChannelFlutterSingBox();
+  const MethodChannel channel = MethodChannel('flutter_sing_box');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        return '42';
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+
+}
