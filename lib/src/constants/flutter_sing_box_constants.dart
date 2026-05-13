6fe11d0589cf3ba07aa45a0daf5b7fc420d7085e
flutter-lib <flutterlib@gmail.com>
2026-05-13 19:39:08 +0800

    feat: 添加 defaultTestUrl 常量并格式化代码
    - 在 FlutterSingBoxConstants 中新增 defaultTestUrl 默认测试链接
    - 调整 templateConfig 的代码换行格式
    - 补充文件末尾的换行符

--- lib/src/constants/flutter_sing_box_constants.dart
+++ lib/src/constants/flutter_sing_box_constants.dart
@@ -1,6 +1,8 @@
 class FlutterSingBoxConstants {
-  static const templateConfig = 'packages/flutter_sing_box/assets/configs/singbox_config_template.json';
+  static const templateConfig =
+      'packages/flutter_sing_box/assets/configs/singbox_config_template.json';
   static const String localFilePrefix = 'file://';
   static const List<String> localSubscriptionFileExtensions = ['yaml', 'json', 'txt'];
   static const String defaultGroup = 'proxy';
+  static const String defaultTestUrl = 'https://www.gstatic.com/generate_204';
 }
