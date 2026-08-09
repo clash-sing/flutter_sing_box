bc52b3492401b47fa2b941550941e21749e303ac
flutter-lib <flutterlib@gmail.com>
2026-08-10 02:06:46 +0800

    chore: 升级 Windows 端 sing-box 版本至 1.13.18

--- lib/flutter_sing_box_windows.dart
+++ lib/flutter_sing_box_windows.dart
@@ -166,7 +166,7 @@ class FlutterSingBoxWindows extends FlutterSingBoxPlatform {
 
   @override
   Future<String> getSingBoxVersion() async {
-    return '1.13.15';
+    return '1.13.18';
   }
 
   @override
