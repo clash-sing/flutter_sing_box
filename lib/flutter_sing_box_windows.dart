575627ee5971b3243a8a3f1684c0414d57585c7e
flutter-lib <flutterlib@gmail.com>
2026-09-15 13:44:56 +0800

    chore(windows): 升级 sing-box 版本号从 1.14.0 到 1.14.1

--- lib/flutter_sing_box_windows.dart
+++ lib/flutter_sing_box_windows.dart
@@ -166,7 +166,7 @@ class FlutterSingBoxWindows extends FlutterSingBoxPlatform {
 
   @override
   Future<String> getSingBoxVersion() async {
-    return '1.14.0';
+    return '1.14.1';
   }
 
   @override
